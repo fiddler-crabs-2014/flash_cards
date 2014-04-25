3cfb14f3735f111df37456baf753f6b04d2543cb
Adam Godel & Shaun McGeever <pair+onweapps.shaunmcgeever@devbootcamp.com>
2014-04-25 16:38:05 -0400

    working on correct methods on guessing

--- flash_cards/app/models/card.rb
+++ flash_cards/app/models/card.rb
@@ -1,4 +1,15 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
   # Remember to create a migration!
+
+
+  def correct_answer?(guess)
+    #@card = Card.where(question: question)
+    if @answer == guess
+      return true
+    else
+      return false
+    end
+  end
+
 end
--- flash_cards/app/models/round.rb
+++ flash_cards/app/models/round.rb
@@ -2,4 +2,13 @@ class Round < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :deck
   belongs_to :user
+
+  def add_score
+    @score += 5
+  end
+  def subtract_score
+    @score -= 5
+  end
 end
+
+
