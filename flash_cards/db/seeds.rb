847c1cdc5aada3e9244dcdf129c7571202c971e3
nealfennimore <fennimoren@gmail.com>
2014-04-26 15:31:39 -0400

    Added Deck img tables, created img folder and moved imgs there

--- flash_cards/db/seeds.rb
+++ flash_cards/db/seeds.rb
@@ -1,11 +1,11 @@
-ruby_deck = Deck.create(name: "ruby_deck")
+ruby_deck = Deck.create(name: "ruby_deck", img: "ruby.png")
 
 card_file = File.readlines('./db/rubydeck.txt')
 card_file.each_slice(3) do |slice|
   Card.create({question: slice[0].chop, answer: slice[1].chop, deck_id: ruby_deck.id})
 end
 
-chuck_norris_deck = Deck.create(name: "chuck_norris")
+chuck_norris_deck = Deck.create(name: "chuck_norris", img: 'chucknorris.png')
 chuck_norris = File.readlines('./db/migrate/chuck-norris.txt')
 chuck_norris.each do |line|
   line.gsub!("Chuck Norris", "________")
