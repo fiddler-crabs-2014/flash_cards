ec404308c7d1b1fa0db8318d0b8d9b489f78d7e7
Brendan & Neal Fennimore <pair+Onaracs.nealfennimore@devbootcamp.com>
2014-04-26 13:49:19 -0400

    chuck norris deck, adam's additional work

--- flash_cards/app/controllers/decks.rb
+++ flash_cards/app/controllers/decks.rb
@@ -5,22 +5,27 @@ get "/decks_display" do
 end
 
 get "/go_to_deck/:id" do
+  #@cards = params[:cards]
   unless session[:round_id]
+    puts "HIT ___________________________"
     @round =  Round.create({user_id: session[:user_id], deck_id: params[:id]})
     session[:round_id] = @round.id
+    puts "CARD CLASS #{@cards.class}"
   end
   @deck_id = params[:id]
-
   @cards = Card.where("deck_id = ?", @deck_id)
+  #@cards = Card.where("deck_id = ?", @deck_id)
+  #puts "CARD COUNT: #{@cards.length}"
 
-  @sample = @cards.sample
+  @sample = @cards.shuffle.pop
   @question = @sample.question
   @answer = @sample.answer
   @id = @sample.id
   #@question = sample.question
   #@answer = sample.answer
-  puts "SAMPLE INSPECT: #{@sample.inspect}"
-  puts "SAMPLE question: #{@sample.question}"
+  #puts "SAMPLE INSPECT: #{@sample.inspect}"
+  #puts "SAMPLE question: #{@sample.question}"
+  @cards
   erb :go_to_deck
 end
 
@@ -29,7 +34,7 @@ end
 post '/answer' do
   card_id = params[:card_id]
   @card = Card.find(card_id)
-  puts "CARD OBJECT: #{@card.inspect}"
+  #puts "CARD OBJECT: #{@card.inspect}"
   answer = params[:answer]
   @round = Round.find(session[:round_id])
   @deck_id =  @round.deck_id
